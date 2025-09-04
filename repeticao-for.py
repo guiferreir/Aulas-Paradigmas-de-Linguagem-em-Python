54b89e4f2e05322db227fb5d34c7285559d2d5a2
guilcus <guilhermeferreiracoliveira@gmail.com>
2025-09-03 22:09:41 -0300

    include atividades 03/09/25

---
+++ repeticao-for.py
@@ -0,0 +1,10 @@
+for n in range (1, 10): # Itens um abaixo do outro
+    print(n)
+
+for n in range (1, 10): # Itens na mesma linha
+    print(n, end=' ')
+    
+print() # Pula uma linha
+
+for n in range (1, 10, 2): # Itens de 2 em 2
+    print(n, end=' ')
--- vai-uma-pizza.py
+++ vai-uma-pizza.py
@@ -1,8 +1,5 @@
 qtd_amigos = int(input("Digite a quantidade de amigos: "))
-pizza = qtd_amigos // 8 # o "//" é uma divisao inteira
-
-# Quando a divisao for exata
-# Quando a divisao nao for exata
+pizza = qtd_amigos // 8
 
 if qtd_amigos % 8 == 0:
     print(pizza)
